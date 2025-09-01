fa9bbcc8f677ab3552def312d90598960869df78
SpaceEstate <patrick.comper@yahoo.com>
2025-09-01 16:49:30 +0200

    Update get-session.js

--- checkin/api/get-session.js
+++ checkin/api/get-session.js
@@ -3,6 +3,15 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "https://spaceestate.github.io");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { session_id } = req.query;
 
   try {
